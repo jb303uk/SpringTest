6a1a39ee86fed8c47fdc7456f4bca555a43e768e
James Beardsley <jb303@hotmail.com>
2023-10-08 17:33:46 +0100

    mvn archetype:generate

---
+++ mvn_archetype.sh
@@ -0,0 +1,9 @@
+mvn archetype:generate                      \
+   "-DarchetypeGroupId=io.cucumber"           \
+   "-DarchetypeArtifactId=cucumber-archetype" \
+   "-DarchetypeVersion=7.14.0"               \
+   "-DgroupId=uk.co.jb303"                  \
+   "-DartifactId=springtest"               \
+   "-Dpackage=springtest"                  \
+   "-Dversion=0.1.0-SNAPSHOT"                 \
+   "-DinteractiveMode=false"
